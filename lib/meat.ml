978c0981f654255b193bc97e6464ef028ee741aa
Jared Kane <jared.kane@hungerstation.com>
2024-08-16 20:42:56 +0300

    almost to 19

--- lib/meat.ml
+++ lib/meat.ml
@@ -2,6 +2,10 @@ type 'a node =
   | One of 'a
   | Many of 'a node list
 
+type 'a rle =
+  | Modone of 'a
+  | Modmany of int * 'a
+
 let rec last = function
   | [] -> None
   | x :: [] -> Some x
@@ -77,14 +81,82 @@ let pack ls =
 ;;
 
 let encode ls =
-  let rec aux counter item acc = function
-    | [] when counter = 0 -> []
-    | [] -> rev ((counter, item) :: acc)
-    | hd :: tl when counter = 0 -> aux 1 hd acc tl
-    | hd :: tl when hd = item -> aux (counter + 1) item acc tl
-    | hd :: tl -> aux 1 hd ((counter, item) :: acc) tl
+  let rec aux counter acc = function
+    | [] -> []
+    | [ x ] -> rev ((counter + 1, x) :: acc)
+    | hd :: md :: tl when hd = md -> aux (counter + 1) acc (md :: tl)
+    | hd :: tl -> aux 0 ((counter + 1, hd) :: acc) tl
   in
-  aux 0 "" [] ls
+  aux 0 [] ls
+;;
+
+let mod_encode ls =
+  let rec aux counter acc = function
+    | [] -> []
+    | [ x ] when counter = 0 -> rev (Modone x :: acc)
+    | [ x ] -> rev (Modmany (counter + 1, x) :: acc)
+    | hd :: md :: tl when hd = md -> aux (counter + 1) acc (md :: tl)
+    | hd :: tl when counter = 0 -> aux 0 (Modone hd :: acc) tl
+    | hd :: tl -> aux 0 (Modmany (counter + 1, hd) :: acc) tl
+  in
+  aux 0 [] ls
+;;
+
+let decode ls =
+  let rec add_many num_needed item idx acc =
+    if num_needed = idx then acc else add_many num_needed item (idx + 1) (item :: acc)
+  in
+  let rec aux acc = function
+    | [] -> rev acc
+    | Modone x :: tl -> aux (x :: acc) tl
+    | Modmany (n, x) :: tl -> aux (add_many n x 0 acc) tl
+  in
+  aux [] ls
+;;
+
+let duplicate ls =
+  let rec aux acc = function
+    | [] -> rev acc
+    | hd :: tl -> aux (hd :: hd :: acc) tl
+  in
+  aux [] ls
+;;
+
+let replicate ls num =
+  let rec add_many n x acc = if n = 0 then acc else add_many (n - 1) x (x :: acc) in
+  let rec aux acc = function
+    | [] -> rev acc
+    | hd :: tl -> aux (add_many num hd [] @ acc) tl
+  in
+  aux [] ls
+;;
+
+let drop ls n =
+  let rec aux idx acc = function
+    | [] -> rev acc
+    | _ :: tl when idx = 1 -> aux n acc tl
+    | hd :: tl -> aux (idx - 1) (hd :: acc) tl
+  in
+  aux n [] ls
+;;
+
+let split ls n =
+  let rec aux idx acc = function
+    | [] -> rev acc, []
+    | ls when idx = 0 -> rev acc, ls
+    | hd :: tl -> aux (idx - 1) (hd :: acc) tl
+  in
+  aux n [] ls
+;;
+
+let slice ls i k =
+  let rec aux counter acc = function
+    | [] -> rev acc
+    | _ when counter > k -> rev acc
+    | hd :: tl ->
+      if counter >= i then aux (counter + 1) (hd :: acc) tl else aux (counter + 1) acc tl
+  in
+  aux 0 [] ls
 ;;
 
 let () = ()
--- tests/test.ml
+++ tests/test.ml
@@ -22,6 +22,14 @@ let tests =
  ["e"; "e"; "e"; "e"]] (pack ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "d"; "e"; "e"; "e"; "e"]))
        ; ("run-length encoding" >:: fun _ -> assert_equal [(4, "a"); (1, "b"); (2, "c"); (2, "a"); (1, "d"); (4, "e")]
  (encode ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"]))
+       ; ("modified run-length encoding" >:: fun _ -> assert_equal [Modmany (4, "a"); Modone "b"; Modmany (2, "c"); Modmany (2, "a"); Modone "d";
+ Modmany (4, "e")] (mod_encode ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"]))
+       ; ("modified run-length decoding" >:: fun _ -> assert_equal ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"] (decode [Modmany (4, "a"); Modone "b"; Modmany (2, "c"); Modmany (2, "a"); Modone "d"; Modmany (4, "e")]))
+       ; ("duplicate list" >:: fun _ -> assert_equal ["a"; "a"; "b"; "b"; "c"; "c"; "c"; "c"; "d"; "d"] (duplicate ["a"; "b"; "c"; "c"; "d"]))
+       ; ("duplicate list n times" >:: fun _ -> assert_equal ["a"; "a"; "a"; "b"; "b"; "b"; "c"; "c"; "c"] (replicate ["a"; "b"; "c"] 3))
+       ; ("drop every nth item" >:: fun _ -> assert_equal ["a"; "b"; "d"; "e"; "g"; "h"; "j"] (drop ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"; "i"; "j"] 3))
+       ; ("split at n" >:: fun _ -> assert_equal (["a"; "b"; "c"], ["d"; "e"; "f"; "g"; "h"; "i"; "j"]) (split ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"; "i"; "j"] 3))
+       ; ("splice at i and k both inclusive" >:: fun _ -> assert_equal (["c"; "d"; "e"; "f"; "g"]) (slice ["a"; "b"; "c"; "d"; "e"; "f"; "g"; "h"; "i"; "j"] 2 6))
          (* "!! 4" >:: (fun _ -> assert_equal 24 (!! 4) ~cmp:Int.equal ~printer:string_of_int); *)
          (* "!! -4" >:: (fun _ -> assert_raises (Failure "not a positive int") (fun () -> (!! (-4)))); *)
          (* "4 or more events of 80% out of 5" >:: (fun _ -> assert_equal 0.73728 (culm_probs 4 5 0.8) ~cmp:( Float.approx_equal ) ~printer:string_of_float); *)
