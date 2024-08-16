6e9c2457d7991f3171c63887904ba5a0092370ce
Jared Kane <jared.kane@hungerstation.com>
2024-08-16 18:11:55 +0300

    12ish done

--- lib/meat.ml
+++ lib/meat.ml
@@ -1,3 +1,7 @@
+type 'a node =
+  | One of 'a
+  | Many of 'a node list
+
 let rec last = function
   | [] -> None
   | x :: [] -> Some x
@@ -14,6 +18,73 @@ let rec nth ls idx =
   match ls with
   | [] -> raise (Failure "nth")
   | hd :: tl -> if idx == 0 then hd else nth tl (idx - 1)
+;;
+
+let length ls =
+  let rec aux counter = function
+    | [] -> counter
+    | _ :: tl -> aux (counter + 1) tl
+  in
+  aux 0 ls
+;;
+
+let rev ls =
+  let rec aux acc = function
+    | [] -> acc
+    | hd :: tl -> aux (hd :: acc) tl
+  in
+  aux [] ls
+;;
+
+let flatten ls =
+  let rec aux acc = function
+    | [] -> acc
+    | One hd :: tl -> aux (hd :: acc) tl
+    | Many hd :: tl ->
+      let new_acc = aux acc hd in
+      aux new_acc tl
+  in
+  aux [] ls |> rev
+;;
+
+let palindrone ls =
+  let sl = rev ls in
+  let rec aux ls sl =
+    match ls, sl with
+    | [], [] -> true
+    | hd :: tl, dh :: lt when hd = dh -> aux tl lt
+    | _, _ -> false
+  in
+  aux ls sl
+;;
+
+let compress ls =
+  let rec aux acc = function
+    | [] -> rev acc
+    | a :: b :: tl when a = b -> aux acc (a :: tl)
+    | a :: tl -> aux (a :: acc) tl
+  in
+  aux [] ls
+;;
+
+let pack ls =
+  let rec aux inner outer = function
+    | [] -> rev outer
+    | a :: b :: tl when a = b -> aux (a :: inner) outer (b :: tl)
+    | a :: tl -> aux [] ((a :: inner) :: outer) tl
+  in
+  aux [] [] ls
+;;
+
+let encode ls =
+  let rec aux counter item acc = function
+    | [] when counter = 0 -> []
+    | [] -> rev ((counter, item) :: acc)
+    | hd :: tl when counter = 0 -> aux 1 hd acc tl
+    | hd :: tl when hd = item -> aux (counter + 1) item acc tl
+    | hd :: tl -> aux 1 hd ((counter, item) :: acc) tl
+  in
+  aux 0 "" [] ls
 ;;
 
 let () = ()
--- tests/test.ml
+++ tests/test.ml
@@ -8,10 +8,20 @@ let tests =
           >:: fun _ -> assert_equal (Some "d") (last [ "a"; "b"; "c"; "d" ]))
        ; ("last two items in string"
           >:: fun _ -> assert_equal (Some ("c", "d")) (last_two [ "a"; "b"; "c"; "d" ]))
-       ; ("nth item in string"
+       ; ("nth item in list"
           >:: fun _ -> assert_equal "c" (nth [ "a"; "b"; "c"; "d"; "e" ] 2))
-       ; ("nth item in string"
+       ; ("nth item in list"
           >:: fun _ -> assert_raises (Failure "nth") (fun () -> nth [ "a" ] 2))
+       ; ("length of a list" >:: fun _ -> assert_equal 3 (length [ "a"; "b"; "c" ]))
+       ; ("reverse a list" >:: fun _ -> assert_equal ["c"; "b"; "a"] (rev [ "a"; "b"; "c" ]))
+       ; ("palindrone" >:: fun _ -> assert_equal true (palindrone ["x"; "a"; "m"; "a"; "x"]))
+       ; ("not palindrone" >:: fun _ -> assert_equal false (palindrone ["x"; "a"]))
+       ; ("flat list" >:: fun _ -> assert_equal ["a"; "b"; "c"; "d"; "e"] (flatten [One "a"; Many [One "b"; Many [One "c" ;One "d"]; One "e"]]))
+       ; ("dedups consecutive" >:: fun _ -> assert_equal ["a"; "b"; "c"; "a"; "d"; "e"] (compress ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"]))
+       ; ("packing dups" >:: fun _ -> assert_equal [["a"; "a"; "a"; "a"]; ["b"]; ["c"; "c"]; ["a"; "a"]; ["d"; "d"];
+ ["e"; "e"; "e"; "e"]] (pack ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "d"; "e"; "e"; "e"; "e"]))
+       ; ("run-length encoding" >:: fun _ -> assert_equal [(4, "a"); (1, "b"); (2, "c"); (2, "a"); (1, "d"); (4, "e")]
+ (encode ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"]))
          (* "!! 4" >:: (fun _ -> assert_equal 24 (!! 4) ~cmp:Int.equal ~printer:string_of_int); *)
          (* "!! -4" >:: (fun _ -> assert_raises (Failure "not a positive int") (fun () -> (!! (-4)))); *)
          (* "4 or more events of 80% out of 5" >:: (fun _ -> assert_equal 0.73728 (culm_probs 4 5 0.8) ~cmp:( Float.approx_equal ) ~printer:string_of_float); *)
